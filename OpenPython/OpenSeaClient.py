1c4ada45f149530473ea9615fdead8bb2e99e18d
Gavin Newcomer <gjnprivate@gmail.com>
2021-12-11 15:31:55 -0600

    pushing opensea sdk

---
+++ OpenPython/OpenSeaClient.py
@@ -0,0 +1,49 @@
+import os
+from typing import Tuple
+
+import requests
+
+class OpenSeaClient():
+
+    def __init__(self):
+        self.base_url = 'https://api.opensea.io/api/v1/'
+        self.session = requests.Session()
+        self.api_key = os.environ['OpenSeaClient']
+        self.headers = ''
+        if self.api_key is not None:
+            self.headers = {'X-API-KEY': self.api_key}
+    
+    def build_url(self, params: dict) -> Tuple[bool, str]:
+        owner = params.get("owner", None)
+        if owner is None:
+            return False, "Owner Cannot Be None"
+        module = params.get("modules", None)
+        if module is None:
+            return False, "Module Cannot Be None"
+        order_by = params.get("order_by", "sale_date")
+        order_direction = params.get("order_direction", "desc")
+        offset = params.get("offset", 0)
+        limit = abs(params.get("limit", 50))
+        if limit > 50 or limit == 0:
+            limit = 50
+        url = f'{self.base_url}{module}/?owner={owner}&order_by={order_by}&order_direction={order_direction}&offset={offset}&limit={limit}'
+        return True, url
+    
+    def get(self, url: str) -> Tuple[bool, dict]:
+        response = self.session.get(url, self.headers)
+        if response.status_code == 200:
+            return True, response.json()
+        else:
+            return False, response.json()
+    
+    def get_address_assets(self, params: dict) -> Tuple[bool, str, dict]:
+        did_pass, url = self.build_url(params)
+        if did_pass is False:
+            return did_pass, str, {}
+        request_did_succeed, response = self.get(url)
+        if request_did_succeed is False:
+            return request_did_succeed, "request failed",response
+        else:
+            return request_did_succeed, "success", response
+        
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='openpython',
+    version='0.0.3',
+    author='Gavin Newcomer',
+    author_email='gjnprivate@gmail.com',
+    description='OpenSea Python SDK',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/gavinnewcomer/OpenPython',
+    project_urls = {
+        "Open Python": "https://github.com/gavinnewcomer/OpenPython/issues"
+    },
+    license='MIT',
+    packages=['OpenPython'],
+    install_requires=['requests'],
+)
