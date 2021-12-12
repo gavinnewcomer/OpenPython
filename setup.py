c029ae079b876ea26110735506a80ac928550f91
Gavin Newcomer <gjnprivate@gmail.com>
2021-12-12 11:50:37 -0600

    version change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='openpython',
-    version='0.0.4',
+    version='0.0.5',
     author='Gavin Newcomer',
     author_email='gjnprivate@gmail.com',
     description='OpenSea Python SDK',
